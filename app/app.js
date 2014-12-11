e6c8d8db6fa0678f5900cc2463d3f0f27de165bc
Manuel Corrales <manuel.corrales@globant.com>
2014-12-11 15:26:26 -0300

    Refactored application adapter out of the app file to its own file

--- app/app.js
+++ app/app.js
@@ -2,28 +2,13 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-import DS from 'ember-data';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
-
-var inflector = Ember.Inflector.inflector;
-
-inflector.irregular('search', 'search');
 
 var App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver,
-  ApplicationAdapter: DS.RESTAdapter.extend({
-    host: 'https://api.spotify.com',
-    namespace: 'v1',
-    inflector: inflector,
-    ajax: function(url, method, hash) {
-      hash.crossDomain = true;
-      hash.xhrFields = {withCredentials: true};
-      return this._super(url, method, hash);
-    }
-  })
+  Resolver: Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
